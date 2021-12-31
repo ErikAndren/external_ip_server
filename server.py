bc59b0fef5f5a0af02d93f9b56780b523fff8cd3
Erik Andrén Zachrisson <erik.andren@gmail.com>
2021-12-31 15:15:39 +0100

    Return plaintext response

--- server.py
+++ server.py
@@ -7,16 +7,9 @@ serverPort = 8080
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
-#        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<html>", "utf-8"))
-#        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-#        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-#        self.wfile.write(bytes("<p>Client ip: %s:%s</p>" % self.client_address, "utf-8"))
         self.wfile.write(bytes("%s" % self.client_address[0], "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
